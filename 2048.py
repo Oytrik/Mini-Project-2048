7d280e0e68f00e928a5a86c538b3e56893192a1b
Oytrik <31069960+Oytrik@users.noreply.github.com>
2017-11-19 21:46:23 +0530

    Add files via upload

--- 2048.py
+++ 2048.py
@@ -14,6 +14,7 @@ clock = pygame.time.Clock()
 A=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 points=0
 hscore=0
+w=x=y=z=1
 def initial():    
     a=[2,2,2,4]
     global points,hscore
@@ -34,7 +35,7 @@ def initial():
         iny2=random.choice(y)   
     A[inx1][iny1]=m    
     A[inx2][iny2]=n
-print (A)
+    print (A)
 def draw():
     global hscore,points
     for i in range (0,4):
@@ -46,103 +47,177 @@ def draw():
     screen.blit(textsurface,(680,100))
     textsurface = myfont.render(str(hscore), False, WHITE)
     screen.blit(textsurface,(680,200))
+    m=n=o=0      
+    for i in range(0,4):
+        for j in range(0,3):
+            if A[i][j]==A[i][j+1]:
+                m=1
+    for i in range(0,4):
+        for j in range(0,3):
+            if A[j][i]==A[j+1][i]:
+                n=1
+    for i in range(0,4):
+        for j in range(0,4):
+            if A[i][j]==0:
+                o=1                   
+    if m==0 and n==0 and o==0:
+        textsurface = myfont.render("Game Over !!!", False, RED)
+        screen.blit(textsurface,(300,50))
 def right():
-    global points,hscore		
-    for r in range(0,4):            
-            for c in range(0,3):
-                if(A[r][c]!=0 and A[r][c+1]==0):
-                    A[r][c+1]=A[r][c]
-                    A[r][c]=0
-                    for i in reversed(range(1,c+1)):
-                        A[r][i]=A[r][i-1]
-                        A[r][i-1]=0               
-            for c in reversed(range(1,4)):
-                if(A[r][c]!=0 and A[r][c]==A[r][c-1]):
-                    A[r][c]=A[r][c]*2
-                    points=points+A[r][c]
-                    A[r][c-1]=0
-                    for k in reversed(range(0,c)):
-                        for i in reversed(range(1,k+1)):
+    global w,points,hscore
+    w=0		
+    for i in range (0,4):
+        for j in range (0,4):
+            if A[i][j] != 0:
+                for k in range(j+1,4):
+                    if A[i][k] == 0:
+                        w=1
+                        break
+                    else:
+                        try:
+                            if A[i][j] == A[i][j+1]:
+                                w=1
+                                break
+                        except:
+                            w=0    
+    if w==1:
+        for r in range(0,4):            
+                for c in range(0,3):
+                    if(A[r][c]!=0 and A[r][c+1]==0):
+                        A[r][c+1]=A[r][c]
+                        A[r][c]=0
+                        for i in reversed(range(1,c+1)):
                             A[r][i]=A[r][i-1]
-                            A[r][i-1]=0                    
+                            A[r][i-1]=0               
+                for c in reversed(range(1,4)):
+                    if(A[r][c]!=0 and A[r][c]==A[r][c-1]):
+                        A[r][c]=A[r][c]*2
+                        points=points+A[r][c]
+                        A[r][c-1]=0
+                        for k in reversed(range(0,c)):
+                            for i in reversed(range(1,k+1)):
+                                A[r][i]=A[r][i-1]
+                                A[r][i-1]=0                    
                  
-    if hscore<points:
-        hscore=points
-    add()
+        if hscore<points:
+            hscore=points
+        add()
 def left():
-    global points,hscore
-    for r in range(0,4):
-            for c in reversed(range(1,4)):
-                if(A[r][c]!=0 and A[r][c-1]==0):
-                    A[r][c-1]=A[r][c]
-                    A[r][c]=0
-                    for i in range(c,3):
-                        A[r][i]=A[r][i+1]
-                        A[r][i+1]=0
-                
-               
-            for c in range(0,3):
-                if(A[r][c]!=0 and A[r][c]==A[r][c+1]):
-                    A[r][c]=A[r][c]*2
-                    points=points+A[r][c]
-                    A[r][c+1]=0
-                    for k in range(c+1,4):
-                        for i in range(k,3):
+    global x,points,hscore
+    x=0		
+    for i in range (0,4):
+        for j in range (0,4):
+            if A[i][j] != 0:
+                for k in range(0,j):
+                    if A[i][k] == 0:
+                        x=1
+                        break
+                    else:
+                        try:
+                            if A[i][j] == A[i][j-1]:
+                                x=1
+                                break
+                        except:
+                            x=0   
+    if x==1:
+        for r in range(0,4):
+                for c in reversed(range(1,4)):
+                    if(A[r][c]!=0 and A[r][c-1]==0):
+                        A[r][c-1]=A[r][c]
+                        A[r][c]=0
+                        for i in range(c,3):
                             A[r][i]=A[r][i+1]
-                            A[r][i+1]=0
+                            A[r][i+1]=0               
+                for c in range(0,3):
+                    if(A[r][c]!=0 and A[r][c]==A[r][c+1]):
+                        A[r][c]=A[r][c]*2
+                        points=points+A[r][c]
+                        A[r][c+1]=0
+                        for k in range(c+1,4):
+                            for i in range(k,3):
+                                A[r][i]=A[r][i+1]
+                                A[r][i+1]=0
             
-    if hscore<points:
-        hscore=points
-    add()             
+        if hscore<points:
+            hscore=points
+        add()             
 def up():
-    global points,hscore
-    for c in range(0,4):
-            for r in reversed(range(1,4)):
-                if(A[r][c]!=0 and A[r-1][c]==0):
-                    A[r-1][c]=A[r][c]
-                    A[r][c]=0
-                    for i in range(r,3):
-                        A[i][c]=A[i+1][c]
-                        A[i+1][c]=0  
-                          
-                    
-            for r in range(0,3):
-                if(A[r][c]!=0 and A[r][c]==A[r+1][c]):
-                    A[r][c]=A[r][c]*2
-                    A[r+1][c]=0
-                    points=points+A[r][c]
-                    for k in range(r+1,4):
-                        for i in range(k,3):
+    global y,points,hscore
+    y=0		
+    for i in range (0,4):
+        for j in range (0,4):
+            if A[j][i] != 0:
+                for k in range(0,j):
+                    if A[k][i] == 0:
+                        y=1
+                        break
+                    else:
+                        try:
+                            if A[j][i] == A[j-1][i]:
+                                y=1
+                                break
+                        except:
+                            y=0    
+    if y==1:
+        for c in range(0,4):
+                for r in reversed(range(1,4)):
+                    if(A[r][c]!=0 and A[r-1][c]==0):
+                        A[r-1][c]=A[r][c]
+                        A[r][c]=0
+                        for i in range(r,3):
                             A[i][c]=A[i+1][c]
-                            A[i+1][c]=0                
-    if hscore<points:
-        hscore=points
-    add()
+                            A[i+1][c]=0                   
+                for r in range(0,3):
+                    if(A[r][c]!=0 and A[r][c]==A[r+1][c]):
+                        A[r][c]=A[r][c]*2
+                        A[r+1][c]=0
+                        points=points+A[r][c]
+                        for k in range(r+1,4):
+                            for i in range(k,3):
+                                A[i][c]=A[i+1][c]
+                                A[i+1][c]=0                
+        if hscore<points:
+            hscore=points
+        add()
 def down():
-    global points,hscore
-    for c in range(0,4):
-            for r in range(0,3):
-                if(A[r][c]!=0 and A[r+1][c]==0):
-                    A[r+1][c]=A[r][c]
-                    A[r][c]=0
-                    for i in reversed(range(1,r+1)):
-                        A[i][c]=A[i-1][c]
-                        A[i-1][c]=0
-                
-               
-            for r in reversed(range(1,4)):
-                if(A[r][c]!=0 and A[r-1][c]==A[r][c]):
-                    A[r][c]=A[r][c]*2
-                    A[r-1][c]=0
-                    points=points+A[r][c]
-                    for k in reversed(range(0,r)):
-                        for i in reversed(range(1,k+1)):
+    global z,points,hscore
+    z=0		
+    for i in range (0,4):
+        for j in range (0,4):
+            if A[j][i] != 0:
+                for k in range(j+1,4):
+                    if A[k][i] == 0:
+                        z=1
+                        break
+                    else:
+                        try:
+                            if A[j][i] == A[j+1][i]:
+                                z=1
+                                break
+                        except:
+                            z=0    
+    if z==1:
+        for c in range(0,4):
+                for r in range(0,3):
+                    if(A[r][c]!=0 and A[r+1][c]==0):
+                        A[r+1][c]=A[r][c]
+                        A[r][c]=0
+                        for i in reversed(range(1,r+1)):
                             A[i][c]=A[i-1][c]
-                            A[i-1][c]=0
+                            A[i-1][c]=0          
+                for r in reversed(range(1,4)):
+                    if(A[r][c]!=0 and A[r-1][c]==A[r][c]):
+                        A[r][c]=A[r][c]*2
+                        A[r-1][c]=0
+                        points=points+A[r][c]
+                        for k in reversed(range(0,r)):
+                            for i in reversed(range(1,k+1)):
+                                A[i][c]=A[i-1][c]
+                                A[i-1][c]=0
                 
-    if hscore<points:
-        hscore=points
-    add()
+        if hscore<points:
+            hscore=points
+        add()
 def add():
     a=[2,4]
     m=random.choice(a)
